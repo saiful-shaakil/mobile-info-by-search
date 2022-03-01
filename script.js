1855275046117d02b3eb2d42889f1fa00b1841e3
si-shakil <sishakil850@gmail.com>
2022-03-01 10:33:40 +0600

    fifth commit

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.getElementById("button-addon2").addEventListener("click", () => {
 });
 const displayPhone = (phones) => {
   phones.forEach((phone) => {
-    console.log(phone);
     const displaySection = document.getElementById("display-section");
     const newPhone = document.createElement("div");
     newPhone.classList.add("col");
@@ -19,11 +18,44 @@ const displayPhone = (phones) => {
                 <h5 class="card-title">${phone.brand}</h5>
                 <p class="card-text">
                     ${phone.phone_name} </br>
-                    <button onclick="displayPhoneDetails()" class="more-button">More</button> 
+                    <button onclick="phoneDetails('${phone.slug}')" class="more-button">More</button> 
                 </p>
                 </div>
             </div>
     `;
     displaySection.appendChild(newPhone);
   });
+};
+const phoneDetails = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    .then((res) => res.json())
+    .then((result) => displayPhoneDetails(result.data));
+};
+const displayPhoneDetails = (phone) => {
+  console.log(phone);
+  const phoneDetailsSection = document.getElementById("display-phone-details");
+  phoneDetailsSection.textContent = "";
+  const mobile = document.createElement("div");
+  mobile.classList.add("col");
+  mobile.innerHTML = `
+    <div class="card h-100">
+                <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="..." />
+                <div class="card-body">
+                <h5 class="card-title">${phone.name}</h5>
+                <p> <span class="title">Brand:</span> ${phone.brand} </br>
+                <span class="title">Release Date:</span> ${phone.releaseDate} </br>
+                <span class="title">Storage:</span> ${phone.mainFeatures.storage} </br>
+                <span class="title">Display Size:</span> ${phone.mainFeatures.displaySize} </br>
+                <span class="title">Chipset:</span> ${phone.mainFeatures.chipSet} </br>
+                <span class="title">Memory:</span> ${phone.mainFeatures.memory} </br>
+                <span class="title">WLAN:</span> ${phone.others.WLAN} </br>
+                <span class="title">Bluetooth:</span> ${phone.others.Bluetooth} </br>
+                <span class="title">GPS:</span> ${phone.others.GPS} </br>
+                <span class="title">NFC:</span> ${phone.others.NFC} </br>
+                <span class="title">Radio:</span> ${phone.others.Radio} </br>
+                <span class="title">USB:</span> ${phone.others.USB} </br>
+                </div>
+            </div>
+    `;
+  phoneDetailsSection.appendChild(mobile);
 };
